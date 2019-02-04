32edb979e49a2e9509be7ebcb2fc541f4e0884d9
Shubham Gaur <shubham_gaur@hotmail.com>
2019-02-04 11:01:15 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,16 +18,7 @@ jQuery(document).ready(function( $ ) {
     $(".navbar-collapse.collapse").removeClass('in');
   });
 });
-var checkScrollBars = function(){
-    var b = $('body');
-    var normalw = 0;
-    var scrollw = 0;
-    if(b.prop('scrollHeight')>b.height()){
-        normalw = window.innerWidth;
-        scrollw = normalw - b.width();
-        $('#container').css({marginRight:'-'+scrollw+'px'});
-    }
-}
+
 /* File: fixed.js
  * Fix shifting fixed navbar to the right 
  */
